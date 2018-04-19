9dfb904c37ca7a0eeafbacafd8cbb1942265fe4c
Jon Ludlam <jonathan.ludlam@citrix.com>
2018-04-19 14:14:30 +0100

    WIP: Initial commit of NBD sparse stuff

--- src/image.ml
+++ src/image.ml
@@ -12,21 +12,24 @@ let startswith prefix x =
 type t = [
 	| `Vhd of string
 	| `Raw of string
+	| `Nbd of string * string
 ]
 
 let to_string = function
 	| `Vhd x -> "vhd:" ^ x
 	| `Raw x -> "raw:" ^ x
+	| `Nbd (x,y) -> Printf.sprintf "nbd:(%s,%s)" x y
 
 let of_device path =
-	try 
+	try
 		match Tapctl.of_device (Tapctl.create ()) path with
 		| _, _, (Some ("vhd", vhd)) -> Some (`Vhd vhd)
 		| _, _, (Some ("aio", vhd)) -> Some (`Raw vhd)
 		| _, _, _ -> raise Not_found
-	with Tapctl.Not_blktap ->
-		None
+	with
+	| Tapctl.Not_blktap ->
+		Some (`Nbd ("foo","bar"))
 	| Tapctl.Not_a_device ->
 		None
-	| _ -> 
+	| _ ->
 		None
